05fadbcbaf4fad68aa3476b61e08ad53d0c402ea
Yeshuwaa <joshua.castillo@ciit.edu.ph>
2024-11-05 20:10:24 +0800

    User Landing Page (Front-End)
    - Added Landing Page
    - Added Landing.css
    - Modified Login.css

---
+++ landing.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GRP3. - Incident Management Systems</title>
+
+    <!-- File Links -->
+	<link rel="icon" href="src/img/favicon/favicon-32x32.png" type="image/x-icon"/>
+    <link href="https://fonts.googleapis.com/css?family=Heebo:400,500,700|Fira+Sans:600" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/cb15e73258.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="src/css/Help Center/landing.css">
+
+</head>
+<body>
+    <div class="header">
+        <div class="logo">Help Center</div>
+        <div class="profile-icon">
+            <i class="fa-solid fa-user"></i>
+        </div>
+    </div>
+        
+    <img src="src/img/login-background-image.jpg" alt="Background Image" class="background-image">
+    <div class="search-container">
+        <div class="searchText">Welcome to the Help Center</div>
+        <div class="search">
+            <div class="fa fa-search"></div>
+            <input type="text" placeholder="Search"/>
+            <div class="fa fa-times"></div>
+        </div>
+    </div>
+    <div class="portalText">Portals</div>
+    <div class="portal-container"> 
+        <div class="portals">
+            <div class="portal-button" onclick="">
+                <div class="portal-links">
+                    <a>Customer Service</a> 
+                    <a1>Welcome! You can ask questions or raise concern by choosing options provided.</a1> 
+                </div>
+            </div>
+            <div class="portal-button" onclick=""> 
+                <div class="portal-links">
+                    <a>Technical Support</a> 
+                    <a1>Welcome! You can raise a request for technical assistance using the options provided.</a1>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
